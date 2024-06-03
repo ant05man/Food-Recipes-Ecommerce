57f2e5bb8a7e6f8ee1239a44c50e3ce8ce06a33b
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-02 21:19:46 -0400

    changed react-dom to root

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import RecipePage from './components/pages/RecipePage';
 import RecipeDetailPage from './components/pages/RecipeDetailPage';
@@ -11,11 +11,13 @@ const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/recipes" component={RecipePage} />
-        <Route path="/recipes/:id" component={RecipeDetailPage} />
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipes" element={<RecipePage />} />
+          <Route path="/recipes/:id" element={<RecipeDetailPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
       </Router>
     </AuthProvider>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'; // Import your main App component
+import { createRoot } from 'react-dom/client';
+import './styles.css';
+import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
