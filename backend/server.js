1a4ec8a31883dcc6f73cc5ef1a972a77ef8506fe
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-04 21:49:17 -0400

    edited server page

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,7 @@ mongoose.connect(process.env.MONGO_URI, {
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/recipes', recipeRoutes);
-app.use('api/mealPlans', mealPlanRoutes);
-
+app.use('/api/mealPlans', mealPlanRoutes);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
