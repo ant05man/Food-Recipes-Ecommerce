8a21f4a05de81097e40149ce25cbcd2d780c4cf9
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-04 20:24:07 -0400

    edited recipeRoutes.js

--- backend/routes/recipeRoutes.js
+++ backend/routes/recipeRoutes.js
@@ -1,27 +1,77 @@
+// routes/recipeRoutes.js
+
 const express = require('express');
 const router = express.Router();
-const { 
-  getRecipes, 
-  getRecipeById, 
-  createRecipe, 
-  updateRecipe, 
-  deleteRecipe 
-} = require('../controllers/recipeController');
-const { protect } = require('../middleware/authMiddleware');
+const Recipe = require('../models/Recipe');
 
 // Get all recipes
-router.get('/', getRecipes);
-
-// Get a single recipe by ID
-router.get('/:id', getRecipeById);
+router.get('/', async (req, res) => {
+  try {
+    const recipes = await Recipe.find();
+    res.json(recipes);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 
 // Create a new recipe
-router.post('/', protect, createRecipe);
+router.post('/', async (req, res) => {
+  const recipe = new Recipe(req.body);
+  try {
+    const newRecipe = await recipe.save();
+    res.status(201).json(newRecipe);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
-// Update a recipe
-router.put('/:id', protect, updateRecipe);
+// Get one recipe
+router.get('/:id', getRecipe, (req, res) => {
+  res.json(res.recipe);
+});
 
-// Delete a recipe
-router.delete('/:id', protect, deleteRecipe);
+// Update one recipe
+router.patch('/:id', getRecipe, async (req, res) => {
+  if (req.body.name != null) {
+    res.recipe.name = req.body.name;
+  }
+  if (req.body.ingredients != null) {
+    res.recipe.ingredients = req.body.ingredients;
+  }
+  if (req.body.instructions != null) {
+    res.recipe.instructions = req.body.instructions;
+  }
+  try {
+    const updatedRecipe = await res.recipe.save();
+    res.json(updatedRecipe);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// Delete one recipe
+router.delete('/:id', getRecipe, async (req, res) => {
+  try {
+    await res.recipe.remove();
+    res.json({ message: 'Recipe deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+async function getRecipe(req, res, next) {
+  let recipe;
+  try {
+    recipe = await Recipe.findById(req.params.id);
+    if (recipe == null) {
+      return res.status(404).json({ message: 'Cannot find recipe' });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+
+  res.recipe = recipe;
+  next();
+}
 
 module.exports = router;
