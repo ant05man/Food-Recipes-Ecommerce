6fb5286a423613339d09c02b91d2cfdffc07258c
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-04 20:38:59 -0400

    modified Recipe.js schema to adjust to mealplanner

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -1,80 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const Recipe = require('../models/Recipe');
+// models/Recipe.js
 
-// GET all recipes
-router.get('/', async (req, res) => {
-  try {
-    const recipes = await Recipe.find();
-    res.json(recipes);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+const mongoose = require('mongoose');
+
+const recipeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  ingredients: [{ type: String, required: true }], // Modified to store ingredients as an array of strings
+  instructions: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
 });
 
-// GET a single recipe by ID
-router.get('/:id', async (req, res) => {
-  try {
-    const recipe = await Recipe.findById(req.params.id);
-    if (!recipe) {
-      return res.status(404).json({ message: 'Recipe not found' });
-    }
-    res.json(recipe);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
+const Recipe = mongoose.model('Recipe', recipeSchema);
 
-// POST a new recipe
-router.post('/', async (req, res) => {
-  const recipe = new Recipe({
-    name: req.body.name,
-    ingredients: req.body.ingredients,
-    instructions: req.body.instructions,
-  });
-  try {
-    const newRecipe = await recipe.save();
-    res.status(201).json(newRecipe);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// PATCH (update) a recipe
-router.patch('/:id', async (req, res) => {
-  try {
-    const recipe = await Recipe.findById(req.params.id);
-    if (!recipe) {
-      return res.status(404).json({ message: 'Recipe not found' });
-    }
-    if (req.body.name != null) {
-      recipe.name = req.body.name;
-    }
-    if (req.body.ingredients != null) {
-      recipe.ingredients = req.body.ingredients;
-    }
-    if (req.body.instructions != null) {
-      recipe.instructions = req.body.instructions;
-    }
-    const updatedRecipe = await recipe.save();
-    res.json(updatedRecipe);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-});
-
-// DELETE a recipe
-router.delete('/:id', async (req, res) => {
-  try {
-    const recipe = await Recipe.findById(req.params.id);
-    if (!recipe) {
-      return res.status(404).json({ message: 'Recipe not found' });
-    }
-    await recipe.remove();
-    res.json({ message: 'Recipe deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
-module.exports = router;
+module.exports = Recipe;
