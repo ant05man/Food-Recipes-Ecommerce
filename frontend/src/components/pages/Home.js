7e8fe125c459830ec972a37e62706a6a694d0399
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-04 21:15:44 -0400

    added links to login and register page in homepage

--- frontend/src/components/pages/Home.js
+++ frontend/src/components/pages/Home.js
@@ -12,7 +12,13 @@ const Homepage = () => {
           <li>
             <Link to="/mealplanner">Meal Planner</Link>
           </li>
-          {/* Add more navigation links */}
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+          {/*  more navigation links as needed */}
         </ul>
       </nav>
     </div>
