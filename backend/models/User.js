a6a95b471fddb6fbc59e071434d8022043aeb014
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-06 19:12:04 -0400

    edited User.js model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,28 @@
+// models/User.js
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
+// Define the user schema
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  username: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  password: { 
+    type: String, 
+    required: true 
+  },
+}, {
+  timestamps: true
 });
 
+// Hash the password before saving the user
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
     return next();
@@ -16,10 +32,16 @@ userSchema.pre('save', async function (next) {
   next();
 });
 
+// Method to compare the entered password with the hashed password
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+// Create indexes for email and username
+userSchema.index({ email: 1 });
+userSchema.index({ username: 1 });
+
+// Create the User model
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
