8a52a85ef27ae9951b4457b29abe56e6d5a58ce2
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-07 06:55:45 -0400

    changed backend sever

--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,30 @@
+// backend/server.js
+
 const express = require('express');
-const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const path = require('path');
-const authRoutes = require('./routes/authRoutes');
+const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const recipeRoutes = require('./routes/recipeRoutes');
-const mealPlanRoutes = require('./routes/mealPlanRoutes');
-dotenv.config();
 
 const app = express();
-
-app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
-
-mongoose.connection.on('connected', () => {
-  console.log('MongoDB connected');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(`MongoDB connection error: ${err}`);
-});
-
-// API routes
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRoutes);
-app.use('/api/recipes', recipeRoutes);
-app.use('/api/mealPlans', mealPlanRoutes);
-
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static(path.join(__dirname, 'frontend/build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Middleware
+app.use(bodyParser.json());
+
+// Use Routes
+app.use('/api/users', userRoutes); // Route for users
+app.use('/api/recipes', recipeRoutes); // Route for recipes
+
+// MongoDB Connection
+mongoose.connect('mongodb://localhost:27017/FoodRecipesEcommerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.log(err));
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
