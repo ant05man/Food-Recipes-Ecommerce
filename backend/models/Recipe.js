11106527c40b6e619548d1cf49fb8f4603e8841c
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-08 19:29:41 -0400

    changed structure of recipe model

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -1,14 +1,23 @@
-// models/Recipe.js
-
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  ingredients: [{ type: String, required: true }], // Modified to store ingredients as an array of strings
-  instructions: { type: String, required: true },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  name: {
+    type: String,
+    required: true
+  },
+  ingredients: {
+    type: [String],
+    required: true
+  },
+  instructions: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
 });
 
-const Recipe = mongoose.model('Recipe', recipeSchema);
-
-module.exports = Recipe;
+module.exports = mongoose.model('Recipe', recipeSchema);
