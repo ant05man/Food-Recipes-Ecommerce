c0a9addc3debe28a6defaa3b5f4d213cc82379a2
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-09 21:11:35 -0400

    added login info to authService

--- frontend/src/components/services/authService.js
+++ frontend/src/components/services/authService.js
@@ -24,3 +24,20 @@ export const register = async (userData) => {
     throw error;
   }
 };
+
+export const login = async (email, password) => {
+  const response = await fetch('http://localhost:5000/api/users/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    throw new Error(errorData.message || 'Login failed');
+  }
+
+  return response.json();
+};
