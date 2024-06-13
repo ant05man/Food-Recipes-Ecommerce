0b2a996161a51243a8921eacf7ee2e5a317b49e7
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-12 20:43:33 -0400

    created page to create new recipes

---
+++ frontend/src/components/pages/CreateRecipePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import RecipeForm from '../RecipeForm';
+
+const CreateRecipePage = () => {
+  return (
+    <div>
+      <h1>Create New Recipe</h1>
+      <RecipeForm />
+    </div>
+  );
+};
+
+export default CreateRecipePage;
