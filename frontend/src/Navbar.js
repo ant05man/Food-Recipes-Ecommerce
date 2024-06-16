48673edfddd48ad65696d9f7e7875815088e7f8e
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-16 16:51:16 -0400

    created navbar

---
+++ frontend/src/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const Navbar = () => {
+  const { user, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/recipes">Recipes</Link></li>
+        {user ? (
+          <>
+            <li><Link to="/profile">Profile</Link></li>
+            <li><button onClick={handleLogout}>Logout</button></li>
+          </>
+        ) : (
+          <>
+            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/register">Register</Link></li>
+          </>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
