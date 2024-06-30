cbd507ed0d1212c4b00c3f47326de489584cadc0
Anthony Littlejohn <ant05man@yahoo.com>
2024-06-30 18:40:42 -0400

    edited api route to run locally

--- frontend/src/components/pages/RecipePage.js
+++ frontend/src/components/pages/RecipePage.js
@@ -15,7 +15,7 @@ const RecipePage = () => {
 
   const fetchRecipes = async () => {
     try {
-      const response = await fetch(`${apiUrl}/api/recipes`);
+      const response = await fetch(`http://localhost:5000/api/recipes`);
       if (!response.ok) {
         throw new Error('Failed to fetch recipes');
       }
