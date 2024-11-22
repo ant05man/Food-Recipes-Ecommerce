1ab5a3bc995a190f28435cde5193bac3b11eb826
Anthony Littlejohn <ant05man@yahoo.com>
2024-11-22 13:52:37 -0500

    limited cookies

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,8 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json( { limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 
 console.log('Connecting to MongoDB...');
